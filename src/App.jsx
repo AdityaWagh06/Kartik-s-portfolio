57e51d539633fd91eaa56bc04a8a19b592c25aee
AdityaWagh06 <adityawagh2525@gmail.com>
2024-12-26 12:13:16 +0530

    Updated the functionality in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home'; // Ensure this path matches your folder structure
+import Home from './pages/Home'; 
 import Categories from './pages/Categories';
 import CategoryDetails from './pages/CategoryDetails';
 
